85247d5e799d3775cccfacc09243b690f9f135b5
Mithun <126346172+mithun205@users.noreply.github.com>
2024-08-01 18:03:44 +0530

    Update bottom nav.dart

--- lib/components/bottom nav.dart
+++ lib/components/bottom nav.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-
+//
 // ignore: must_be_immutable
 class bottom_navbar extends StatelessWidget {
   void Function(int)? onTabChange;
