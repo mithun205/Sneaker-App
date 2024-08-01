a1455bbe0a99b6e0ffa0a9057d3909c9f4fcaa6c
Mithun <126346172+mithun205@users.noreply.github.com>
2024-08-01 19:59:33 +0530

    Update cart page.dart

--- lib/pages/cart page.dart
+++ lib/pages/cart page.dart
@@ -1,37 +1,74 @@
-import 'package:app_flutt/components/cartITEM.dart';
-import 'package:app_flutt/model/cart.dart';
 import 'package:flutter/material.dart';
+//import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
+import '../components/cartITEM.dart';
+import '../model/cart.dart';
 import '../model/shoe.dart';
 
-class cartpage extends StatelessWidget {
-  const cartpage({super.key});
+class CartPage extends StatelessWidget {
+  const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<cart>(builder: (context, value, child) => 
-    Padding(
-      padding: const EdgeInsets.symmetric(horizontal:25),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text("My Cart",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
-
-          const SizedBox(height: 15),
-
-
-          Expanded(child: ListView.builder(
-            itemCount: value.getusercart().length,
-            itemBuilder: (context, index) {
-            Shoe individualShoe= value.getusercart()[index];
-
-            return CartItem(
-              shoe: individualShoe
-            );
-          },))
-        ],
+    return Consumer<Cart>(
+      builder: (context, cart, child) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 21),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "My Cart",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            const SizedBox(height: 15),
+            Expanded(
+              child: ListView.builder(
+                itemCount: cart.getusercart().length,
+                itemBuilder: (context, index) {
+                  Shoe individualShoe = cart.getusercart()[index];
+                  return CartItem(shoe: individualShoe);
+                },
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Total amnt... 
+                Text(
+                  ' ₹${cart.getTotalAmount()}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                ),
+                // place order button...
+                GestureDetector(
+                  onTap: (){
+                  Navigator.pushNamed(context, "/payment page");
+                },  //
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: const Text(
+                      "Place order",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-    ),);
+    );
   }
 }
